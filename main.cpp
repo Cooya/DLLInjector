243c1e110c966d018dc8e3244f9199d48a082d30
Nicolas <nicolas.marcy@etu.u-bordeaux.fr>
2016-01-15 09:42:56 +0100

    injector ok

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include <windows.h>
+#include <iostream>
+#include <cstdio>
+#include <tlhelp32.h>
+
+HANDLE retrieveProcessId(char *processName) {
+    HANDLE hProcess = NULL;
+    PROCESSENTRY32 entry;
+    entry.dwFlags = sizeof(PROCESSENTRY32);
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS,0);
+    if(Process32First(snapshot,&entry) == true)
+        while(Process32Next(snapshot,&entry) == true)
+            if(_stricmp(entry.szExeFile, processName) == 0)
+                hProcess = OpenProcess(PROCESS_ALL_ACCESS,false,entry.th32ProcessID);
+    CloseHandle(snapshot);
+    return hProcess;
+}
+
+int main() {
+    char* dllPath = (char*) "No.Ankama.dll";
+    char* processName = (char*) "adl.exe";
+
+    void* pLoadLibrary = (void*)GetProcAddress(GetModuleHandle("kernel32"),"LoadLibraryA");
+
+    HANDLE hProcess = NULL;
+    STARTUPINFOA startupInfo;
+    ZeroMemory(&startupInfo,sizeof(startupInfo));
+
+    std::cout << "Getting process ID of process \"" << processName << "\"...\n";
+    if(!(hProcess = retrieveProcessId(processName))) {
+        std::cout << "Process ID unknown. GetLastError() = " << GetLastError();
+        return 0;
+    }
+
+    std::cout << "Allocating virtual memory ...\n";
+    void* pReservedSpace = VirtualAllocEx(hProcess,NULL,strlen(dllPath),MEM_COMMIT,PAGE_EXECUTE_READWRITE);
+    if(!pReservedSpace) {
+        std::cout << "Could not allocate virtual memory. GetLastError() = " << GetLastError();
+        return 0;
+    }
+
+    std::cout << "Writing process memory ...\n";
+    if(!WriteProcessMemory(hProcess,pReservedSpace,dllPath,strlen(dllPath),NULL)) {
+        std::cout << "Error while calling WriteProcessMemory(). GetLastError() = " << GetLastError();
+        return 0;
+    }
+
+    std::cout << "Creating remote thread ...\n";
+    HANDLE hThread = CreateRemoteThread(hProcess,NULL,0,(LPTHREAD_START_ROUTINE)pLoadLibrary,pReservedSpace,0,NULL);
+    if(!hThread) {
+        std::cout << "Unable to create the remote thread. GetLastError() = " << GetLastError();
+        return 0;
+    }
+
+    std::cout << "Thread created.\n";
+
+    WaitForSingleObject(hThread,INFINITE);
+    VirtualFreeEx(hProcess,pReservedSpace,strlen(dllPath),MEM_COMMIT);
+    CloseHandle(hProcess);
+
+    std::cout << "Done.";
+    return 0;
+}
+
+/*
+std::cout << "LoadLibrary:0x" << std::hex << pLoadLibrary << std::dec << "\nCreating process 'TargetApplication.exe' ... \n";
+if(!CreateProcessA(0,"TargetApplication.exe",0,0,1,CREATE_NEW_CONSOLE,0,0,&startupInfo,&processInformation)) {
+    std::cout << "Could not run 'TargetApplication.exe'. GetLastError() = " << GetLastError();
+    return 0;
+}
+*/
